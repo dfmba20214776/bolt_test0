2d893f7b9e7b3891a91f752ab277850e54c2e8e0
dfmba20214776 <git@stackblitz.com>
2024-10-16 11:27:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bolt_test0
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/dfmba20214776/bolt_test0)
---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>한국신용정보원 LLM 비교서비스</h1>
+    <p>다양한 LLM(Large Language Model) 모델을 비교하고 분석하는 서비스입니다.</p>
+  </div>
+`
